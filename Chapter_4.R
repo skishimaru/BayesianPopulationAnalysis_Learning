36b27cdd623ce04f2225b32c4e8e2761ea296a53
skishimaru <157835146+skishimaru@users.noreply.github.com>
2024-03-28 14:01:39 -1000

    Created Chapter_4.R
    Created r-script to run through chapter 4.

---
+++ Chapter_4.R
@@ -0,0 +1,34 @@
+# Bayesian Population Analysis using WinBUGS
+# Chapter 4: Introduction to Random Effects: Conventional Poisson GLMM for Count Data
+
+library(R2jags) #to run JAGS
+library(shinystan) #to run shiny stan
+library(tidyverse) #to utilize pipe operators
+
+# 4.1 Introduction
+# 4.1.1 An Example
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 4.2 Accounting for Overdispersion by Ransom Effects: Modeling in R and WinBUGS
+# 4.2.1 Generation and Analysis of Simulated Data
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 4.2.2 Analysis of Real Data
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 4.3 Mixed Models with Random Effects for Variability in a Group
+# 4.3.1 Generation and Analysis of Simulated Data
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 4.3.2 Analysis of a Real Data Set
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
