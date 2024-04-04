fd811bdb70fbd35543a59ef44f62914f53471dbd
skishimaru <157835146+skishimaru@users.noreply.github.com>
2024-04-04 08:43:26 -1000

    Created Chapter_5.R
    Created r-script to run through chapter 5

---
+++ Chapter_5.R
@@ -0,0 +1,21 @@
+# Bayesian Population Analysis using WinBUGS
+# Chapter 5: State-Space Models for Populations Counts
+
+library(R2jags) #to run JAGS
+library(shinystan) #to run shiny stan
+library(tidyverse) #to utilize pipe operators
+
+# 5.2 A Simple Model
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 5.3 Systematic Bias in the Observation Process
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 5.4 Real Example: House Martin Population Counts in the Village of Magden
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
