d32e4950db50cc5eb836c9a68616b95bd6c67081
skishimaru <157835146+skishimaru@users.noreply.github.com>
2024-04-09 08:23:03 -1000

    Created Chapter_6.R
    Created r-script to run through chapter 6

---
+++ Chapter_6.R
@@ -0,0 +1,48 @@
+# Bayesian Population Analysis using WinBUGS
+# Chapter 6: Estimation of the Size of a Closed Population from Capture-Recapture Data
+
+library(R2jags) #to run JAGS
+library(shinystan) #to run shiny stan
+library(tidyverse) #to utilize pipe operators
+
+# 6.2 Generation and Analysis of Simulated Data with Data Augmentation
+# 6.2.1 Introduction to Data Augmentation for the Simplest Case: Model M0
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 6.2.2 Time Effects: Model Mt
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 6.2.3 Behavioral or Memory Effects: Model Mb
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 6.2.4 Individual (Random) Effects: The Heterogeneity Model Mh
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 6.2.5 Combined Effects (time + random): Model Mth
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 6.3 Analysis of a Real Data Set: Model Mtbh for Species Richness Estimation
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 6.4 Capture-Recapture Models with Individual Covariables: Model Mt+x
+# 6.4.1 Individual Covariate Model for Species Richness Estimation
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 6.4.2 Individual Covariate Model for Population Size Estiamtion
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
