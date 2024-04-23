437df1bdb592f2523431eef444004af0222be871
skishimaru <157835146+skishimaru@users.noreply.github.com>
2024-04-23 09:22:41 -1000

    Created Chapter_8.R
    Created r-script to run through chapter 8

---
+++ Chapter_8.R
@@ -0,0 +1,33 @@
+# Bayesian Population Analysis using WinBUGS
+# Chapter 8: Estimation of Survival Using Mark-Recovery Data
+
+library(R2jags) #to run JAGS
+library(shinystan) #to run shiny stan
+library(tidyverse) #to utilize pipe operators
+
+# 8.2 The Mark-Recovery Model as a State-Space Model
+# 8.2.1 Simulation of Mark-Recovery Data
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 8.2.2 Analysis of a Model with Constant Parameters
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 8.3 The Mark-Recovery Model Fitted with the Multinomial Likelihood
+# 8.3.1 Constant Parameters
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 8.3.2 Age-Dependent Parameters
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 8.4 Real-Data Example: Age-Dependent Survival in Swiss Red Kites
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
