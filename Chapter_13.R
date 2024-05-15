a11d74460a7bf83efcaa3d30ee9ce4dce9d583c0
skishimaru <157835146+skishimaru@users.noreply.github.com>
2024-05-15 10:23:22 -1000

    Created Chapter_13.R
    Created r-script to run through chapter 13

---
+++ Chapter_13.R
@@ -0,0 +1,43 @@
+# Bayesian Population Analysis using WinBUGS
+# Chapter 13: Estimation of Occupancy and Species Distributions from Detection/Nondetection Data in Metapopulation Designs Using Site-Occupancy Models
+
+library(R2jags) #to run JAGS
+library(shinystan) #to run shiny stan
+library(tidyverse) #to utilize pipe operators
+
+# 13.2 What Happens When p<1 and Constant and p is Not Accounted for in a Species Distrbution Model?
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 13.3 Generation and Analysis of Simulated Data for Single-Season Occupancy
+# 13.3.1 The Simplest Possible Site-Occupancy Model
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 13.3.2 Site-Occupancy Models with Covariates
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 13.4 Analysis of A Real Data Set: Single-Season Occupancy Model
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 13.5 Dynamic (Multiseason) Site-Occupancy Models
+# 13.5.1 Generation and Analysis of Simulated Data
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 13.5.2 Dynamic Occupancy Modeling in a REal Data Set
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+
+# 13.6 Multistate Occupancy Models
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
